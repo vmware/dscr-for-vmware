04dc5a748b5b339e8310628ebfaa19081f3fa17f
SimeonGerginov <simeongerginov1@gmail.com>
2018-12-21 09:37:15 +0200

    Removed unused Imports for Compare-Settings.Unit.Tests
    Signed-off-by: SimeonGerginov <simeongerginov1@gmail.com>

--- Source/VMware.vSphereDSC/Tests/Unit/Compare-Settings.Unit.Tests.ps1
+++ Source/VMware.vSphereDSC/Tests/Unit/Compare-Settings.Unit.Tests.ps1
@@ -1,29 +1,22 @@
-using module VMware.vSphereDSC
-$script:modulePath = $env:PSModulePath
-$script:unitTestsFolder = Join-Path (Join-Path (Get-Module VMware.vSphereDSC -ListAvailable).ModuleBase 'Tests') 'Unit'
-$script:moduleName = 'VMware.vSphereDSC'
+<#
+Copyright (c) 2018 VMware, Inc.  All rights reserved
 
+The BSD-2 license (the "License") set forth below applies to all parts of the Desired State Configuration Resources for VMware project.  You may not use this file except in compliance with the License.
+
+BSD-2 License
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#>
+
+Using module '..\..\VMware.vSphereDSC.Helper.psm1'
 
 Describe 'Compare-Settings'{
-
-    BeforeAll {
-        # Arrange
-        $env:PSModulePath = $script:mockModuleLocation
-        $vimAutomationModule = Get-Module -Name VMware.VimAutomation.Core
-        if ($null -ne $vimAutomationModule -and $vimAutomationModule.Path -NotMatch 'TestHelpers')
-        {
-            throw 'The Original VMware.VimAutomation.Core Module is loaded in the current session. If you want to run the unit tests please open a new PowerShell session.'
-        }
-
-        Import-Module -Name VMware.VimAutomation.Core
-        Import-Module .\Source\VMware.vSphereDSC\VMware.vSphereDSC.Helper.psm1
-    }
-
-    AfterAll {
-        Remove-Module -Name VMware.VimAutomation.Core
-        $env:PSModulePath = $script:modulePath
-    }
-
     Context 'Desired and current settings match'{
         $desiredState = @{
             key1 = "value 1"
