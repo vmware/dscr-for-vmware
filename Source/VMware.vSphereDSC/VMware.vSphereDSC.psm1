9adeede4e2ab08a8e1b4fb229146663b5d37b186
Travis CI <travis@travis-ci.org>
2019-03-15 13:03:33 +0000

    Travis update: (Build 162)
    [skip ci]
    
    Signed-off-by: Travis CI <travis@travis-ci.org>

--- Source/VMware.vSphereDSC/VMware.vSphereDSC.psm1
+++ Source/VMware.vSphereDSC/VMware.vSphereDSC.psm1
@@ -59,6 +59,14 @@ enum NicTeamingPolicy {
     Failover_explicit
 }
 
+enum DrsAutomationLevel {
+    FullyAutomated
+    Manual
+    PartiallyAutomated
+    Disabled
+    Unset
+}
+
 enum HAIsolationResponse {
     PowerOff
     DoNothing
@@ -3024,6 +3032,334 @@ class VMHostVssTeaming : VMHostVssBaseDSC {
 }
 
 [DscResource()]
+class DrsCluster : InventoryBaseDSC {
+    DrsCluster() {
+        $this.DatacenterFolderType = [DatacenterFolderType]::Host
+    }
+
+    <#
+    .DESCRIPTION
+
+    Indicates that VMware DRS (Distributed Resource Scheduler) is enabled.
+    #>
+    [DscProperty()]
+    [nullable[bool]] $DrsEnabled
+
+    <#
+    .DESCRIPTION
+
+    Specifies a DRS (Distributed Resource Scheduler) automation level. The valid values are FullyAutomated, Manual, PartiallyAutomated, Disabled and Unset.
+    #>
+    [DscProperty()]
+    [DrsAutomationLevel] $DrsAutomationLevel = [DrsAutomationLevel]::Unset
+
+    <#
+    .DESCRIPTION
+
+    Threshold for generated ClusterRecommendations. DRS generates only those recommendations that are above the specified vmotionRate. Ratings vary from 1 to 5.
+    This setting applies to Manual, PartiallyAutomated, and FullyAutomated DRS Clusters.
+    #>
+    [DscProperty()]
+    [nullable[int]] $DrsMigrationThreshold
+
+    <#
+    .DESCRIPTION
+
+    For availability, distributes a more even number of virtual machines across hosts.
+    #>
+    [DscProperty()]
+    [nullable[int]] $DrsDistribution
+
+    <#
+    .DESCRIPTION
+
+    Load balance based on consumed memory of virtual machines rather than active memory.
+    This setting is recommended for clusters where host memory is not over-committed.
+    #>
+    [DscProperty()]
+    [nullable[int]] $MemoryLoadBalancing
+
+    <#
+    .DESCRIPTION
+
+    Controls CPU over-commitment in the cluster.
+    Min value is 0 and Max value is 500.
+    #>
+    [DscProperty()]
+    [nullable[int]] $CPUOverCommitment
+
+    hidden [string] $DrsEnabledConfigPropertyName = 'Enabled'
+    hidden [string] $DrsAutomationLevelConfigPropertyName = 'DefaultVmBehavior'
+    hidden [string] $DrsMigrationThresholdConfigPropertyName = 'VmotionRate'
+    hidden [string] $DrsDistributionSettingName = 'LimitVMsPerESXHostPercent'
+    hidden [string] $MemoryLoadBalancingSettingName = 'PercentIdleMBInMemDemand'
+    hidden [string] $CPUOverCommitmentSettingName = 'MaxVcpusPerClusterPct'
+
+    [void] Set() {
+        $this.ConnectVIServer()
+
+        $foundDatacenter = $this.GetDatacenterFromPath()
+        $clusterLocation = $this.GetInventoryItemLocationFromPath($foundDatacenter)
+        $cluster = $this.GetInventoryItem($foundDatacenter, $clusterLocation)
+
+        if ($this.Ensure -eq [Ensure]::Present) {
+            if ($null -eq $cluster) {
+                $this.AddCluster($clusterLocation)
+            }
+            else {
+                $this.UpdateCluster($cluster)
+            }
+        }
+        else {
+            if ($null -ne $cluster) {
+                $this.RemoveCluster($cluster)
+            }
+        }
+    }
+
+    [bool] Test() {
+        $this.ConnectVIServer()
+
+        $foundDatacenter = $this.GetDatacenterFromPath()
+        $clusterLocation = $this.GetInventoryItemLocationFromPath($foundDatacenter)
+        $cluster = $this.GetInventoryItem($foundDatacenter, $clusterLocation)
+
+        if ($this.Ensure -eq [Ensure]::Present) {
+            if ($null -eq $cluster) {
+                return $false
+            }
+
+            return !$this.ShouldUpdateCluster($cluster)
+        }
+        else {
+            return ($null -eq $cluster)
+        }
+    }
+
+    [DrsCluster] Get() {
+        $result = [DrsCluster]::new()
+        $result.Server = $this.Server
+        $result.InventoryPath = $this.InventoryPath
+        $result.Datacenter = $this.Datacenter
+
+        $this.ConnectVIServer()
+
+        $foundDatacenter = $this.GetDatacenterFromPath()
+        $clusterLocation = $this.GetInventoryItemLocationFromPath($foundDatacenter)
+        $cluster = $this.GetInventoryItem($foundDatacenter, $clusterLocation)
+
+        $this.PopulateResult($cluster, $result)
+
+        return $result
+    }
+
+    <#
+    .DESCRIPTION
+
+    Checks if the Cluster option should be updated.
+    #>
+    [bool] ShouldUpdateOptionValue($options, $key, $desiredValue) {
+        if ($null -ne $desiredValue) {
+            $currentValue = ($options | Where-Object { $_.Key -eq $key }).Value
+
+            if ($null -eq $currentValue) {
+                return $true
+            }
+            else {
+                return $desiredValue -ne $currentValue
+            }
+        }
+
+        return $false
+    }
+
+    <#
+    .DESCRIPTION
+
+    Checks if the Cluster should be updated.
+    #>
+    [bool] ShouldUpdateCluster($cluster) {
+        $drsConfig = $cluster.ExtensionData.ConfigurationEx.DrsConfig
+
+        $shouldUpdateCluster = @()
+        $shouldUpdateCluster += ($null -ne $this.DrsEnabled -and $this.DrsEnabled -ne $drsConfig.Enabled)
+        $shouldUpdateCluster += ($this.DrsAutomationLevel -ne [DrsAutomationLevel]::Unset -and $this.DrsAutomationLevel.ToString() -ne $drsConfig.DefaultVmBehavior)
+        $shouldUpdateCluster += ($null -ne $this.DrsMigrationThreshold -and $this.DrsMigrationThreshold -ne $drsConfig.VmotionRate)
+
+        if ($null -ne $drsConfig.Option) {
+            $shouldUpdateCluster += $this.ShouldUpdateOptionValue($drsConfig.Option, $this.DrsDistributionSettingName, $this.DrsDistribution)
+            $shouldUpdateCluster += $this.ShouldUpdateOptionValue($drsConfig.Option, $this.MemoryLoadBalancingSettingName, $this.MemoryLoadBalancing)
+            $shouldUpdateCluster += $this.ShouldUpdateOptionValue($drsConfig.Option, $this.CPUOverCommitmentSettingName, $this.CPUOverCommitment)
+        }
+        else {
+            $shouldUpdateCluster += ($null -ne $this.DrsDistribution -or $null -ne $this.MemoryLoadBalancing -or $null -ne $this.CPUOverCommitment)
+        }
+
+        return ($shouldUpdateCluster -Contains $true)
+    }
+
+    <#
+    .DESCRIPTION
+
+    Populates the DrsConfig property with the desired value.
+    #>
+    [void] PopulateDrsConfigProperty($drsConfig, $propertyName, $propertyValue) {
+        <#
+            Special case where the passed property value is enum type. These type of properties
+            should be populated only when their value is not equal to Unset.
+            Unset means that the property was not specified in the Configuration.
+        #>
+        if ($propertyValue -is [DrsAutomationLevel]) {
+            if ($propertyValue -ne [DrsAutomationLevel]::Unset) {
+                $drsConfig.$propertyName = $propertyValue.ToString()
+            }
+        }
+        elseif ($null -ne $propertyValue) {
+            $drsConfig.$propertyName = $propertyValue
+        }
+    }
+
+    <#
+    .DESCRIPTION
+
+    Returns the Option array for the DrsConfig with the specified options in the Configuration.
+    #>
+    [PSObject] GetOptionsForDrsConfig($allOptions) {
+        $drsConfigOptions = @()
+
+        foreach ($key in $allOptions.Keys) {
+            if ($null -ne $allOptions.$key) {
+                $option = New-Object VMware.Vim.OptionValue
+
+                $option.Key = $key
+                $option.Value = $allOptions.$key.ToString()
+
+                $drsConfigOptions += $option
+            }
+        }
+
+        return $drsConfigOptions
+    }
+
+    <#
+    .DESCRIPTION
+
+    Returns the populated Cluster Spec with the specified values in the Configuration.
+    #>
+    [PSObject] GetPopulatedClusterSpec() {
+        $clusterSpec = New-Object VMware.Vim.ClusterConfigSpecEx
+        $clusterSpec.DrsConfig = New-Object VMware.Vim.ClusterDrsConfigInfo
+
+        $this.PopulateDrsConfigProperty($clusterSpec.DrsConfig, $this.DrsEnabledConfigPropertyName, $this.DrsEnabled)
+        $this.PopulateDrsConfigProperty($clusterSpec.DrsConfig, $this.DrsAutomationLevelConfigPropertyName, $this.DrsAutomationLevel)
+        $this.PopulateDrsConfigProperty($clusterSpec.DrsConfig, $this.DrsMigrationThresholdConfigPropertyName, $this.DrsMigrationThreshold)
+
+        $allOptions = @{
+            $this.DrsDistributionSettingName = $this.DrsDistribution
+            $this.MemoryLoadBalancingSettingName = $this.MemoryLoadBalancing
+            $this.CPUOverCommitmentSettingName = $this.CPUOverCommitment
+        }
+
+        $clusterSpec.DrsConfig.Option = $this.GetOptionsForDrsConfig($allOptions)
+
+        return $clusterSpec
+    }
+
+    <#
+    .DESCRIPTION
+
+    Creates a new Cluster with the specified properties at the specified location.
+    #>
+    [void] AddCluster($clusterLocation) {
+        $clusterSpec = $this.GetPopulatedClusterSpec()
+
+        try {
+            Add-Cluster -Folder $clusterLocation.ExtensionData -Name $this.Name -Spec $clusterSpec
+        }
+        catch {
+            throw "Server operation failed with the following error: $($_.Exception.Message)"
+        }
+    }
+
+    <#
+    .DESCRIPTION
+
+    Updates the Cluster with the specified properties.
+    #>
+    [void] UpdateCluster($cluster) {
+        $clusterSpec = $this.GetPopulatedClusterSpec()
+
+        try {
+            Update-ClusterComputeResource -ClusterComputeResource $cluster.ExtensionData -Spec $clusterSpec
+        }
+        catch {
+            throw "Server operation failed with the following error: $($_.Exception.Message)"
+        }
+    }
+
+    <#
+    .DESCRIPTION
+
+    Removes the Cluster from the specified Datacenter.
+    #>
+    [void] RemoveCluster($cluster) {
+        try {
+            Remove-ClusterComputeResource -ClusterComputeResource $cluster.ExtensionData
+        }
+        catch {
+            throw "Server operation failed with the following error: $($_.Exception.Message)"
+        }
+    }
+
+    <#
+    .DESCRIPTION
+
+    Populates the result returned from the Get() method with the values of the Cluster from the server.
+    #>
+    [void] PopulateResult($cluster, $result) {
+        if ($null -ne $cluster) {
+            $drsConfig = $cluster.ExtensionData.ConfigurationEx.DrsConfig
+
+            $result.Name = $cluster.Name
+            $result.Ensure = [Ensure]::Present
+            $result.DrsEnabled = $drsConfig.Enabled
+
+            if ($null -eq $drsConfig.DefaultVmBehavior) {
+                $result.DrsAutomationLevel = [DrsAutomationLevel]::Unset
+            }
+            else {
+                $result.DrsAutomationLevel = $drsConfig.DefaultVmBehavior.ToString()
+            }
+
+            $result.DrsMigrationThreshold = $drsConfig.VmotionRate
+
+            if ($null -ne $drsConfig.Option) {
+                $options = $drsConfig.Option
+
+                $result.DrsDistribution = ($options | Where-Object { $_.Key -eq $this.DrsDistributionSettingName }).Value
+                $result.MemoryLoadBalancing = ($options | Where-Object { $_.Key -eq $this.MemoryLoadBalancingSettingName }).Value
+                $result.CPUOverCommitment = ($options | Where-Object { $_.Key -eq $this.CPUOverCommitmentSettingName }).Value
+            }
+            else {
+                $result.DrsDistribution = $null
+                $result.MemoryLoadBalancing = $null
+                $result.CPUOverCommitment = $null
+            }
+        }
+        else {
+            $result.Name = $this.Name
+            $result.Ensure = [Ensure]::Absent
+            $result.DrsEnabled = $this.DrsEnabled
+            $result.DrsAutomationLevel = $this.DrsAutomationLevel
+            $result.DrsMigrationThreshold = $this.DrsMigrationThreshold
+            $result.DrsDistribution = $this.DrsDistribution
+            $result.MemoryLoadBalancing = $this.MemoryLoadBalancing
+            $result.CPUOverCommitment = $this.CPUOverCommitment
+        }
+    }
+}
+
+[DscResource()]
 class HACluster : InventoryBaseDSC {
     HACluster() {
         $this.DatacenterFolderType = [DatacenterFolderType]::Host
@@ -3147,8 +3483,24 @@ class HACluster : InventoryBaseDSC {
         $shouldUpdateCluster += ($null -ne $this.HAEnabled -and $this.HAEnabled -ne $cluster.HAEnabled)
         $shouldUpdateCluster += ($null -ne $this.HAAdmissionControlEnabled -and $this.HAAdmissionControlEnabled -ne $cluster.HAAdmissionControlEnabled)
         $shouldUpdateCluster += ($null -ne $this.HAFailoverLevel -and $this.HAFailoverLevel -ne $cluster.HAFailoverLevel)
-        $shouldUpdateCluster += ($this.HAIsolationResponse -ne [HAIsolationResponse]::Unset -and $this.HAIsolationResponse -ne $cluster.HAIsolationResponse)
-        $shouldUpdateCluster += ($this.HARestartPriority -ne [HARestartPriority]::Unset -and $this.HARestartPriority -ne $cluster.HARestartPriority)
+
+        if ($this.HAIsolationResponse -ne [HAIsolationResponse]::Unset) {
+            if ($null -ne $cluster.HAIsolationResponse) {
+                $shouldUpdateCluster += ($this.HAIsolationResponse.ToString() -ne $cluster.HAIsolationResponse.ToString())
+            }
+            else {
+                $shouldUpdateCluster += $true
+            }
+        }
+
+        if ($this.HARestartPriority -ne [HARestartPriority]::Unset) {
+            if ($null -ne $cluster.HARestartPriority) {
+                $shouldUpdateCluster += ($this.HARestartPriority.ToString() -ne $cluster.HARestartPriority.ToString())
+            }
+            else {
+                $shouldUpdateCluster += $true
+            }
+        }
 
         return ($shouldUpdateCluster -Contains $true)
     }
@@ -3169,7 +3521,7 @@ class HACluster : InventoryBaseDSC {
                 $clusterParams.$parameter = $desiredValue.ToString()
             }
 
-			return
+            return
         }
 
         if ($null -ne $desiredValue) {
@@ -3235,7 +3587,7 @@ class HACluster : InventoryBaseDSC {
     <#
     .DESCRIPTION
 
-    Removes the specified Cluster from the specified Datacenter.
+    Removes the Cluster from the specified Datacenter.
     #>
     [void] RemoveCluster($cluster) {
         try {
