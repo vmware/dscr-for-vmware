cf2bab8b885dbb71382ee88b51183f49183bba5c
Kristiyan Kamburov <kristiangk@abv.bg>
2020-11-03 11:19:01 +0200

    Build modifications to run the tests only when a module code is changed (#290)
    * Updated build to improve performance by building and testing only when needed.
    Introduced buildFlags enum in Build/common.ps1 which shows the steps of the build that should be performed.
    Added some documentation improvements.
    
    Signed-off-by: Kristiyan <kristiangk@abv.bg>
    
    * Fixed a bug for removing the used travis workspace file.
    
    Signed-off-by: Kristiyan <kristiangk@abv.bg>
    
    * In the Invoke-vSphereDSCTests functions the manifest changes now get reverted after test execution.
    Added more documentation for the build flags.
    Fixed typos.
    
    Signed-off-by: Kristiyan <kristiangk@abv.bg>

--- Build/common.ps1
+++ Build/common.ps1
@@ -15,21 +15,135 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 #>
 
 <#
-.Synopsis
+.DESCRIPTION
+BuildFlags defines operations which the build should perform.
+Update_PSDSC flag runs the build process for VMware.PSDesiredStateConfiguration.
+Update_VSDSC flag runs the build process for VMware.vSphereDSC.
+Tests_VSDSC flag runs the VMware.vSphereDSC unit tests.
+Tests_PSDSC flag runs the VMware.PSDesiredStateConfiguration unit tests.
+None is the defaut value for not running any additional steps.
+#>
+[Flags()] enum BuildFlags {
+    Update_PSDSC = 1
+    Update_VSDSC = 2
+    Tests_VSDSC = 4
+    Tests_PSDSC = 8
+    None = 16
+}
+
+<#
+.SYNOPSIS
+Checks if a certain DesiredFlag is included in the InputFlag.
+.DESCRIPTION
+Checks if a certain DesiredFlag is included in the InputFlag.
+The check is performed using a binary and on flags.
+.PARAMETER InputFlag
+Contains all the flags for the current build.
+.PARAMETER DesiredFlag
+The flag that gets checked if it's contained the current build flags.
+#>
+function Test-Flag {
+    [OutputType([bool])]
+    Param (
+        [BuildFlags]
+        $InputFlag,
+
+        [BuildFlags]
+        $DesiredFlag
+    )
+
+    ($InputFlag -band $DesiredFlag) -ne 0
+}
+
+<#
+.SYNOPSIS
+Finds and returns a list of file paths that have been changed since last build
+.DESCRIPTION
+Finds and returns a list of file paths that have been changed since last build.
+The changes are searched in the .git HEAD from the current commit to the last travis commit.
+#>
+function Get-ChangedFiles {
+    [OutputType([System.String[]])]
+    Param()
+
+    $lastTravisCommit = 1
+
+    # find last travis commit
+    while ($true) {
+        $commitInfo = git show HEAD~$lastTravisCommit
+        $author = $commitInfo[1]
+
+        if ($author.Contains('travis@travis-ci.org')) {
+            break
+        }
+
+        $lastTravisCommit += 1
+    }
+
+    # extract names of changed files from HEAD history
+    $changedFiles = git diff --name-only HEAD..HEAD~$lastTravisCommit
+
+    $changedFiles
+}
+
+<#
+.SYNOPSIS
+Depending on the changes made in the project [BuildFlags] get set and returned.
+.DESCRIPTION
+Depending on the changes made in the project [BuildFlags] get set and returned.
+The returned value contains all steps that should be perfomred depending on build change.
+If there is change in the any of the modules in Source then the changed module build + unit tests get run.
+If there is change in the build process or in .travis.yml the unit tests for all modules get run.
+#>
+function Set-BuildFlags {
+    # retrieve a list of changed files
+    $changedFiles = Get-ChangedFiles
+
+    $flagResult = 0
+
+    foreach ($changedFile in $changedFiles) {
+        if ($changedFile.Contains('Source')) {
+            # module change triggers the it's build and tests to run
+            if ($changedFile.Contains('VMware.PSDesiredStateConfiguration')) {
+                $flagResult = $flagResult -bor [BuildFlags]::Update_PSDSC
+                $flagResult = $flagResult -bor [BuildFlags]::Tests_PSDSC
+            } elseif ($changedFile.Contains('VMware.vSphereDSC')) {
+                $flagResult = $flagResult -bor [BuildFlags]::Update_VSDSC
+                $flagResult = $flagResult -bor [BuildFlags]::Tests_VSDSC
+            }
+        } elseif ($changedFile.Contains('Build') -or $changedFiles.Contains('.travis.yml')) {
+            # build change triggers unit tests to be run
+            $flagResult = $flagResult -bor [BuildFlags]::Tests_PSDSC
+            $flagResult = $flagResult -bor [BuildFlags]::Tests_VSDSC
+        }
+    }
+
+    # no major changes have been made
+    if ($flagResult -eq 0) {
+        $flagResult = [BuildFlags]::None
+    }
+
+    $flagResult
+}
+
+<#
+.SYNOPSIS
 Runs the unit tests for the specified module and returns code coverage percentage.
 
-.Description
-Runs the unit tests for the specified module and returns code coverage percentage. The tests should be located in a Tests\Unit location in the modules directory.
+.DESCRIPTION
+Runs the unit tests for the specified module and returns code coverage percentage. 
+The tests should be located in a Tests\Unit location in the modules directory.
 The code coverage result of the tests gets updated in the README.md document.
+This function relies on Pester for running the unit tests. If the module is not found it gets installed.
 
-.Notes
+.NOTES
 The code coverage logic leads to the unit tests running slower.
 Bug link: https://github.com/pester/Pester/issues/1318
 
-.Parameter ModuleName
+.PARAMETER ModuleName
 Name of the module whose unit tests should be run
 
-.Parameter DisableCodeCoverage
+.PARAMETER DisableCodeCoverage
 Disables code coverage for the unit tests.
 With this flag turned on the function does not return a result.
 #>
@@ -45,6 +159,22 @@ function Invoke-UnitTests {
         [switch]
         $DisableCodeCoverage
     )
+
+    if ($null -eq (Get-Module -Name 'Pester' -ListAvailable)) {
+        # Install Pester.
+        Write-Host 'Installing Pester'
+
+        # suppress progress messages during installation due
+        # to broken display on Travis console
+        $oldProgressPreference = $ProgressPreference
+        $Global:ProgressPreference = 'SilentlyContinue'
+
+        Install-Module -Name Pester -RequiredVersion 4.10.1 -Scope CurrentUser -Force -SkipPublisherCheck
+
+        $Global:ProgressPreference = $oldProgressPreference
+
+        Write-Host 'Pester Installed'   
+    }
 
     # Runs all unit tests in the module.
     $moduleFolderPath = (Get-Module $ModuleName -ListAvailable).ModuleBase
@@ -73,17 +203,14 @@ function Invoke-UnitTests {
     }
 }
 
+# Define script variables
 $script:ProjectRoot = (Get-Item -Path $PSScriptRoot).Parent.FullName
-
-# Adds the Source directory from the repository to the list of modules directories.
 $script:SourceRoot = Join-Path -Path $script:ProjectRoot -ChildPath 'Source'
 $script:ReadMePath = Join-Path -Path $script:ProjectRoot -ChildPath 'README.md'
 $Script:ChangelogDocumentPath = Join-Path -Path $Script:ProjectRoot -ChildPath 'CHANGELOG.md'
 
+# Adds the Source directory from the repository to the list of modules directories.
 $env:PSModulePath += "$([System.IO.Path]::PathSeparator)$script:SourceRoot"
 
 # Registeres default PSRepository.
 Register-PSRepository -Default -ErrorAction SilentlyContinue
-
-# Installs Pester.
-Install-Module -Name Pester -RequiredVersion 4.10.1 -Scope CurrentUser -Force -SkipPublisherCheck
--- Build/enableExperimentalFeature.ps1
+++ Build/enableExperimentalFeature.ps1
@@ -14,12 +14,18 @@ Redistributions in binary form must reproduce the above copyright notice, this l
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #>
 
-Write-Host ('PS VERSION IS:' + $PSVersionTable['psversion'])
+<#
+.NOTES
+The code for enabling the experimental feature is in a separate file
+because the powershell session must be restarted when it is enabled
+#>
+
+Write-Host ('-------PS VERSION IS:' + $PSVersionTable['psversion'])
 
 if ($PSVersionTable['PsEdition'] -eq 'Core') {
     # check if the Invoke-DscResource emperimental feature is enabled
     if (-not [ExperimentalFeature]::IsEnabled('PSDesiredStateConfiguration.InvokeDscResource')) {
         Enable-ExperimentalFeature PSDesiredStateConfiguration.InvokeDscResource
-        Write-Host ('InvokeDscResource is enabled')
+        Write-Host ('-------InvokeDscResource is enabled')
     }
 }
